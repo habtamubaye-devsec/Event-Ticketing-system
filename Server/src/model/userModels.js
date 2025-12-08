4e50a8f59952c590c384b2ffcad4056daba9be98
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-08 03:36:08 +0300

    feat: create user model with schema for user management

---
+++ Server/src/model/userModels.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true 
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    isAdmin: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+}, { timestamps: true});
+
+module.exports = mongoose.model("User", userSchema); 
